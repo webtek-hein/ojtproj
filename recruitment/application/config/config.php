1b8591b85e182cd80432bc9a9faafc19c3d3c204
famaepascua <2143735@slu.edu.ph>
2018-04-05 19:52:52 +0800

    correct spelling

--- recruitment/application/config/config.php
+++ recruitment/application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost/ojtproject/recruitement/';
+$config['base_url'] = 'http://localhost/ojtproject/recruitment/';
 
 $config['index_page'] = '';
 
