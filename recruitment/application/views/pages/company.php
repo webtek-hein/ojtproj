724c870bfb54be28c96ea1a5bc8c70b59e32dc5d
famaepascua <2143735@slu.edu.ph>
2018-04-06 22:45:35 +0800

    give me UI on modal

--- recruitment/application/views/pages/company.php
+++ recruitment/application/views/pages/company.php
@@ -1,11 +1,15 @@
 <!-- ============================================================== -->
 <!-- Start Page Content -->
 <!-- ============================================================== -->
+
 <div class="row">
     <!-- column -->
     <div class="col-sm-12">
         <div class="card">
             <div class="card-block">
+                <!-- Add company -->
+                <button type="button" class="btn btn-info" data-toggle="modal" data-target="#addCompany">Add Company
+                </button>
                 <select class="custom-select pull-right">
                     <option selected>Registered</option>
                     <option value="1">Archived</option>
@@ -33,3 +37,34 @@
 <!-- ============================================================== -->
 <!-- End PAge Content -->
 <!-- ============================================================== -->
+
+<!-- Modal -->
+<div id="addCompany" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+        <form method="POST">
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">New Company</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label class="col-md-12">Full Name</label>
+                        <div class="col-md-12">
+                            <input type="text" placeholder="Johnathan Doe" class="form-control form-control-line">
+                        </div>
+                        <label class="col-md-12">Full Name</label>
+                        <div class="col-md-12">
+                            <input type="text" placeholder="Johnathan Doe" class="form-control form-control-line">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-success" data-dismiss="modal">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
