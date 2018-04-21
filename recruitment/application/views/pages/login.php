5d3f0834e60bc07a6eb8c9e46348a317b00df30f
famaepascua <21437352@alu.edu.ph>
2018-04-22 07:12:25 +0800

    background

--- recruitment/application/views/pages/login.php
+++ recruitment/application/views/pages/login.php
@@ -37,7 +37,7 @@ if (isset($this->session->userdata['logged_in'])) {
 <body background="images/bg-02.jpg">
 
 <div class="limiter">
-    <div class="container-login100" style="background: url('assets/images/fam.jpg'); position:fixed;top:0px;">
+    <div class="container-login100" style="background: url('assets/images/bg-03.jpg'); position:fixed;top:0px;">
         <div class="wrap-login100 p-t-30 p-b-50">
 				<span class="login100-form-title p-b-41">
 					Account Login
