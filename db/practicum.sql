141eb6a1375f0dfbaee4adb4909c531d09e96f7a
Famae Pascua <2143735@slu.edu.ph>
2018-06-05 08:55:17 +0800

    db

--- db/practicum.sql
+++ db/practicum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 02, 2018 at 05:50 AM
+-- Generation Time: Jun 04, 2018 at 11:51 PM
 -- Server version: 5.7.21
 -- PHP Version: 5.6.35
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `practicum`
 --
+CREATE DATABASE IF NOT EXISTS `practicum` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `practicum`;
 
 -- --------------------------------------------------------
 
@@ -34,16 +36,8 @@ CREATE TABLE IF NOT EXISTS `appointment` (
   `user_id` int(11) NOT NULL,
   `sched_id` int(11) NOT NULL,
   `appointment_date` date NOT NULL,
-  `status` varchar(45) NOT NULL DEFAULT 'ongoing',
   PRIMARY KEY (`appt_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `appointment`
---
-
-INSERT INTO `appointment` (`appt_id`, `user_id`, `sched_id`, `appointment_date`, `status`) VALUES
-(1, 1, 1, '2018-05-16', 'ongoing');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -66,19 +60,7 @@ CREATE TABLE IF NOT EXISTS `company` (
   `status` varchar(250) NOT NULL DEFAULT 'registered',
   `image_url` varchar(250) NOT NULL,
   PRIMARY KEY (`company_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `company`
---
-
-INSERT INTO `company` (`company_id`, `contact_person`, `address`, `company_name`, `suffix`, `email`, `tel_num`, `mobile_num`, `alt_number`, `about`, `status`, `image_url`) VALUES
-(1, 'test test test', '#1,ortigas  Manila,Pasig City', 'Heinrich Micro ', 'Mr', 'test@gmail.com', '06545646846', '06453468', '09847689', 'Trend Micro Inc. is a Japanese multinational cyber security & defense company founded in Los Angeles, California with global headquarters in Tokyo, Japan, a R&D center in Taipei, Taiwan, and regional headquarters in Asia, Europe and the Americas.', 'registered', 'Trend-Micro-Logo.png'),
-(2, '  ', '#1200,MSE Building, Ayala Ave. Metro Manila,Makati', 'Accenture', 'Mr', '', '(02) 841 0111', '', '', 'Accenture solves our clients\' toughest challenges by providing unmatched services in strategy, consulting, digital, technology and operations. We partner with more than three-quarters of the Fortune Global 500, driving innovation to improve the way the world works and lives.', 'registered', 'accenture.png'),
-(3, '  ', '#,Nokia-Manila Technology Center? Building I UP Ayala Land Technohub? Commonweal', 'Nokia', 'Mr', 'nokia@gmail.com', '', '+63 28-577-000?', '', 'Nokia is a global leader in innovations such as mobile networks, digital health and phones. See how we create technology to connect.', 'registered', 'nokia.png'),
-(4, 'Heinrich 1 ba', '#1,f test,ag', 'Legit', 'Mr', 'heinrichbangui@gmail.com', '41121', '421421', '231241', 'dsc', 'registered', '17274043_1357756327581124_1455685824_o.jpg'),
-(5, 'Heinrich 1 ba', '#1,f test,ag', 't1', 'Mrs', 'heinrichbangui@gmail.com', '41121', '09997653851', '231241', 'des', 'registered', '17274043_1357756327581124_1455685824_o1.jpg'),
-(6, 'Hein 11 heiehi', '#1,213 test,test', 'Mycom', 'Mr', 'dummy@gmail.com', '41121', '421421', '231241', 'desc', 'registered', '17274043_1357756327581124_1455685824_o2.jpg');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -93,32 +75,23 @@ CREATE TABLE IF NOT EXISTS `logs` (
   `activity` text NOT NULL,
   `user_id` int(15) NOT NULL,
   PRIMARY KEY (`logsID`)
-) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `logs`
+-- Table structure for table `messages`
 --
 
-INSERT INTO `logs` (`logsID`, `timestamp`, `activity`, `user_id`) VALUES
-(1, '2018-06-02 03:41:14', 'something', 1),
-(2, '2018-06-02 04:02:02', 'Text here', 2),
-(3, '2018-06-02 05:24:58', 'Accepted user 2151287', 2),
-(4, '2018-06-02 05:25:02', 'Rejected user 2114464', 2),
-(5, '2018-06-02 05:26:39', 'Archived user 1.', 2),
-(6, '2018-06-02 05:29:33', 'Archived user Denyse Cayadi.', 2),
-(7, '2018-06-02 05:31:10', 'Updated information of Bangui Heinrich.', 2),
-(8, '2018-06-02 05:32:13', 'Mycom as new company.', 2),
-(9, '2018-06-02 05:33:10', 'Archived user .', 2),
-(10, '2018-06-02 05:34:52', 'Archived user .', 2),
-(11, '2018-06-02 05:39:30', 'Trend Micro has been archived.', 2),
-(12, '2018-06-02 05:39:40', 'Archived user Bangui Heinrich.', 2),
-(13, '2018-06-02 05:40:48', 'Mycom has been reverted.', 2),
-(14, '2018-06-02 05:40:52', 't1 has been reverted.', 2),
-(15, '2018-06-02 05:40:54', 't has been reverted.', 2),
-(16, '2018-06-02 05:43:06', 'Updated information of t', 2),
-(17, '2018-06-02 05:44:03', 'Added exam schedule for Internshipunder Trend Micro', 2),
-(18, '2018-06-02 05:44:52', 'Updated information of Trend Micro', 2),
-(19, '2018-06-02 05:49:33', 'Updated exam schedule for Internship under Heinrich Micro .', 2);
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE IF NOT EXISTS `messages` (
+  `msgID` int(15) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `message` text NOT NULL,
+  `userID` int(15) NOT NULL,
+  `senderID` int(15) NOT NULL,
+  PRIMARY KEY (`msgID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -140,19 +113,7 @@ CREATE TABLE IF NOT EXISTS `schedule` (
   `slots` int(6) NOT NULL,
   `defaultSlot` int(15) NOT NULL,
   PRIMARY KEY (`sched_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `schedule`
---
-
-INSERT INTO `schedule` (`sched_id`, `company_id`, `sched_type`, `event_type`, `sched_date`, `start_time`, `end_time`, `location`, `room`, `slots`, `defaultSlot`) VALUES
-(1, 2, 'Exam', 'Internship', '2018-06-02', '09:00', '11:00', 'Saint Louis University Maryheights Campus', 'D524', 49, 50),
-(2, 1, 'Exam', 'Employment', '2018-06-04', '08:00', '09:30', 'Saint Louis University Maryheaights Campus', 'D424', 45, 45),
-(3, 3, 'Exam', 'Seminar', '2018-06-08', '13:00', '16:00', 'Prince Bernhard Gym, Saint Louis University', '', 300, 300),
-(4, 1, 'Exam', 'Seminar', '2018-06-28', '08:00', '13:00', 'SLU Maryheights', 'AVR', 200, 200),
-(5, 5, 'Exam', 'Internship', '2018-06-02', '01:00', '01:01', 'tsett', '1', 1, 1),
-(6, 1, 'Exam', 'Internship', '2018-06-02', '15:02', '04:01', 'tsett', '1', 1, 1);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -181,9 +142,9 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`user_id`, `password`, `first_name`, `last_name`, `email`, `id_num`, `contact_num`, `course`, `year`, `user_type`, `status`) VALUES
-(1, '1234', 'Famae', 'Pascua', 'famaepascua@gmail.com', 2143735, '09099299181', 'BSIT', 4, 'student', 'archived'),
+(1, '1234', 'Famae', 'Pascua', 'famaepascua@gmail.com', 2143735, '09099299181', 'BSIT', 4, 'student', 'registered'),
 (2, '1234', 'Admin', 'Admin', 'admin@gmail.com', 123456, '05845488', '', 0, 'admin', 'registered'),
-(3, '1234', 'Denyse', 'Cayadi', 'denyse@gmail.com', 2146790, '', 'BSIT', 4, 'student', 'archived'),
+(3, '1234', 'Denyse', 'Cayadi', 'denyse@gmail.com', 2146790, '', 'BSIT', 4, 'student', 'alumni'),
 (4, '1234', 'Bangui', 'Heinrich', 'heinrichbangui@gmail.com', 2151287, '09994467878', 'BSIT', 10, 'student', 'archived'),
 (5, '1234', 'dummy', 'Dummy', 'dummy@gmail.com', 2114464, '0977754454', 'BSIT', 4, 'student', 'rejected');
 COMMIT;
